35ad42de849f7613e91f09969a255fb8f16c07e8
Iulian Clita <iulian.clita@gmail.com>
2016-09-04 00:15:34 +0300

    Centered reCAPTCHA box

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -56,7 +56,11 @@
                     </div>
                   </div>
 
-                  {!! app('captcha')->display(); !!}
+                  <div class="row">
+                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3">
+                      {!! app('captcha')->display() !!}
+                    </div>
+                  </div>
 
                 </form>
               </div>
